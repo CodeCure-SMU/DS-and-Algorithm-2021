0831e3644cd36385cb024950762c94e17bd80f6d
Jnayoung <82528171+Jnayoung@users.noreply.github.com>
2021-05-17 18:34:04 +0900

    Add files via upload

---
+++ Nayoung/Beakjoon_1021_JeonNayoung.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<deque>
+using namespace std;
+
+deque<int>dq;
+
+int main() {
+	int n, m;
+	int left, right;
+	int cnt = 0;
+
+	cin >> n >> m;
+
+	for (int i = 1; i <= n; i++)
+		dq.push_back(i);
+
+	while (m--) {
+		int num;
+		cin >> num;
+		
+		for (int i = 0; i < dq.size(); i++) {
+			if (dq[i] == num) {
+				left = i;
+				right = dq.size() - i;
+				break;
+			}
+		}
+
+		if (left <= right) {
+			while (1) {
+				if (dq.front() == num) break;
+				dq.push_back(dq.front());
+				dq.pop_front();
+				cnt++;
+			}
+			dq.pop_front();
+		}
+
+		else {
+			cnt++;
+			while (1) {
+				if (dq.back() == num) break;
+				dq.push_front(dq.back());
+				dq.pop_back();
+				cnt++;
+			}
+			dq.pop_back();
+		}
+	}
+	cout << cnt;
+
+	return 0;
+}
